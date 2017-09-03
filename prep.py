0d93a0dff18165c36788a140af40208ec48d505f
Milan Keca <mkeca@renderedtext.com>
2017-09-03 22:53:23 +0200

    Transform sentences to arrays of words

--- prep.py
+++ prep.py
@@ -1,19 +1,21 @@
 from os import listdir
 from os.path import join
 
+import re
+
 def file_paths(data_path):
   return [join(data_path, name) for name in listdir(data_path)]
 
 def training_data(data_path):
   paths = file_paths(data_path)
-  raw_text = [ open(path, 'r').read() for path in paths]
+  raw_text = [open(path, 'r').read() for path in paths]
 
   dataX = []
   dataY = []
   for text in raw_text:
     data = split_data(text)
-    dataX.append(data[0])
-    dataY.append(data[1])
+    dataX.append(split_to_words(data[0]))
+    dataY.append(split_to_words(data[1]))
 
   return dataX, dataY
 
@@ -27,3 +29,6 @@ def split_data(text):
   output_text = '\n'.join(lines)
 
   return input_text, output_text
+
+def split_to_words(sentence):
+    return re.findall(r"[\w']+|[.,!?;:()/\[\]]/", sentence)
