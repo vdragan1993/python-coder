55233d604a54040f1f7740e0834254c6df7c8eeb
Milan Keca <mkeca@renderedtext.com>
2017-09-17 12:51:39 +0200

    Calculate average bleu and code compilance for predictions

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,28 @@
+import numpy as np
 import ast
 from nltk.translate import bleu_score
 
+from prep import split_to_words
+from prep import word_to_index
+from prep import pad_seq
+
+def vec_to_words(predictions, vocab):
+  codes = []
+  for prediction in predictions:
+    code = [vocab[np.argmax(words)] for words in prediction]
+    codes.append(' '.join([word for word in code if word != 'ZERO']))
+
+  return codes
+
+def predict_codes(auto_encoder,
+                  sentences,
+                  input_dict,
+                  input_length,
+                  output_vocab):
+
+  predictions = auto_encoder.predict(sentences)
+
+  return vec_to_words(predictions, output_vocab)
 
 def code_compiles(code):
     """
@@ -14,7 +36,6 @@ def code_compiles(code):
         return False
     return True
 
-
 def calculate_bleu(original, translated):
     """
     Calculates BLEU score for translated code
@@ -25,3 +46,20 @@ def calculate_bleu(original, translated):
     hypothesis = list(translated)
     reference = list(original)
     return bleu_score.sentence_bleu([reference], hypothesis)
+
+def average_bleu(original, translated):
+  sum = 0
+
+  for i in range(0, len(original)):
+    sum += calculate_bleu(original[i], translated[i])
+
+  return sum / len(original)
+
+def average_code_compilance(codes):
+  count = 0
+
+  for code in codes:
+    if code_compiles(code):
+      count += 1
+
+  return count / len(codes)
--- src/generate.py
+++ src/generate.py
@@ -1,17 +1,20 @@
 import argparse
+import numpy as np
 
 from prep import untar
 from prep import prep_data
 
-from transform import prediction_to_code
+from evaluation import predict_codes, vec_to_words, average_bleu, average_code_compilance
 
 from model import get_model
 
 ap = argparse.ArgumentParser()
 ap.add_argument('--tar_path', type=str)
+ap.add_argument('--test_tar_path', type=str)
 ap.add_argument('--dataset', type=str, default='./data')
-ap.add_argument('--x_length', type=int)
-ap.add_argument('--y_length', type=int)
+ap.add_argument('--test', type=str, default='./test')
+ap.add_argument('--x_length', type=int, default=15)
+ap.add_argument('--y_length', type=int, default=20)
 ap.add_argument('--hidden_size', type=int, default=128)
 ap.add_argument('--num_layers', type=int, default=2)
 ap.add_argument('--model_weights', type=str, default='./model_weights.hdf5')
@@ -19,7 +22,9 @@ ap.add_argument('--model_weights', type=str, default='./model_weights.hdf5')
 args = vars(ap.parse_args())
 
 TAR_PATH = args['tar_path']
+TEST_TAR_PATH = args['test_tar_path']
 DATASET_PATH = args['dataset']
+TEST_PATH = args['test']
 INPUT_LENGTH = args['x_length']
 OUTPUT_LENGTH = args['y_length']
 HIDDEN_SIZE = args['hidden_size']
@@ -29,7 +34,12 @@ MODEL_WEIGHTS = args['model_weights']
 if TAR_PATH is not None:
   untar(TAR_PATH)
 
+if TEST_TAR_PATH is not None:
+  untar(TEST_TAR_PATH)
+
 X, Y, input_vocab, output_vocab, input_dict, output_dict = prep_data(DATASET_PATH, INPUT_LENGTH, OUTPUT_LENGTH)
+
+X_test, Y_test, test_input_vocab, test_output_vocab, _, _ = prep_data(TEST_PATH, INPUT_LENGTH, OUTPUT_LENGTH)
 
 auto_encoder = get_model(len(input_vocab),
                          INPUT_LENGTH,
@@ -39,10 +49,12 @@ auto_encoder = get_model(len(input_vocab),
                          NUM_LAYERS)
 auto_encoder.load_weights(MODEL_WEIGHTS)
 
-sentence = 'print false if 1 appears in array [2, 2]'
-code = prediction_to_code(auto_encoder,
-                          sentence,
-                          input_dict,
-                          INPUT_LENGTH,
-                          output_vocab)
-print code
+expected_codes = vec_to_words(Y_test, test_output_vocab)
+codes = predict_codes(auto_encoder,
+                      X_test,
+                      input_dict,
+                      INPUT_LENGTH,
+                      output_vocab)
+
+print(average_bleu(expected_codes, codes))
+print(average_code_compilance(codes))
